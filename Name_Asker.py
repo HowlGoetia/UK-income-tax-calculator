c03e35cf8ba01c16abb656b4bfacd1b0ca841ae0
Tom O'Brien <134745486+HowlGoetia@users.noreply.github.com>
2023-10-09 17:03:45 +0100

    The Python File

---
+++ Name_Asker.py
@@ -0,0 +1,6 @@
+print ("Hello! What is your name?" )
+Name = input()
+if Name == (("Lewis")):
+    print ("Kill yourself you little gingerbread man")
+else:
+    print ("Nice to meet you,",Name,"!" )
